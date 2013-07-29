d4110b09b60f82757fbb8c9fe9e144f22426fb9d
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-29 21:36:19 +0000

    Added feature to kill workers on undeploy.

--- app/recipes/run.rb
+++ app/recipes/run.rb
@@ -11,21 +11,19 @@ directory node['runner_folder_dir'] do
 
 end.run_action(:create)
 
+file_content = "require 'runner-gem'\nrun('#{node['worker_process_name']}')"
+
 # create the runner ruby file (just requires runner-gem)
 file "#{node['runner_file_dir']}" do
   mode '0755'
   owner node['user']
   group node['group']
-  content 'require \'runner-gem\''
+  content file_content
   action :nothing
-
-  only_if do
-    ::File.exists?("#{node['runner_folder_dir']}")
-  end
 
 end.run_action(:create)
 
-# run the code
+# run the code under the user id specified in the stack JSON
 node[:deploy].each do |application, deploy|
   bash 'run_code' do
     code "sudo -u #{node['user']} ruby #{node['runner_file_dir']}"
---
+++ app/recipes/stop.rb
@@ -0,0 +1,2 @@
+# kill the workers
+`pkill -9 -f #{node['worker_process_name']`
