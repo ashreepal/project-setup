de49e1414cb07f3c8247ed1c89282fc38a548a25
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-08-01 16:51:18 +0000

    Minor cleanup including recipe renaming, changing all yml to json, and improving documentation.

--- app/recipes/save_paths.rb
+++ app/recipes/save_paths.rb
@@ -1,15 +1,20 @@
-require 'yaml'
+require 'json'
 
-# records path to important files that will need to be accessed by the runner later
+# records path to important files that will need to be accessed by the runner
+# file when it is run and workers are deployed
 paths = {}
 paths['config'] = node['config_file_dir']
 paths['custom'] = node['custom_file_dir']
 
-node[:deploy].each do |application, deploy|
-  paths['workflows'] = node['workflow-paths'].map { |p| "#{deploy[:deploy_to]}/current/#{p}" }
-  paths['activities'] = node['activity-paths'].map { |p| "#{deploy[:deploy_to]}/current/#{p}" }
+node[:deploy].each do |application, deploy_settings|
+  paths['workflows'] = node['workflow-paths'].map do |p|
+    "#{deploy_settings[:deploy_to]}/current/#{p}"
+  end
+  paths['activities'] = node['activity-paths'].map do |p|
+    "#{deploy_settings[:deploy_to]}/current/#{p}"
+  end
 end
 
 # creates the file
 new_dir(node['paths_folder_dir'], '0755', node['user'], node['group'])
-new_file(node['paths_file_dir'], '0755', node['user'], node['group'], paths.to_yaml, true)
+new_file(node['paths_file_dir'], '0755', node['user'], node['group'], paths.to_json, true)
--- init/attributes/default.rb
+++ init/attributes/default.rb
@@ -5,24 +5,21 @@ TEMP_DIR = '/opt/temp'
 
 # logfile location, to be created recursively
 default['log_folder_dir'] = TEMP_DIR
-default['log_file_dir'] = "#{node['log_folder_dir']}/logfile.yml"
+default['log_file_dir'] = "#{node['log_folder_dir']}/logfile.json"
 
 # configfile location, for use during deployment
 default['config_folder_dir'] = TEMP_DIR
-default['config_file_dir'] = "#{node['config_folder_dir']}/config.yml"
+default['config_file_dir'] = "#{node['config_folder_dir']}/config.json"
 
 # customfile location, for use during deployment
 default['custom_folder_dir'] = TEMP_DIR
-default['custom_file_dir'] = "#{node['custom_folder_dir']}/custom.yml"
+default['custom_file_dir'] = "#{node['custom_folder_dir']}/custom.json"
 
-# pathsfile location, used by run code to find relevant information/parameters
+# pathsfile location, used by runner code to find relevant information/parameters
 default['paths_folder_dir'] = TEMP_DIR
-default['paths_file_dir'] = "#{node['paths_folder_dir']}/paths.yml"
+default['paths_file_dir'] = "#{node['paths_folder_dir']}/paths.json"
 
-# aws directory, to be created recursively
-default['aws_folder_dir'] = '/opt/aws'
-
-# s3 downloaded files ocation, to be created recursively
+# s3 downloaded files location, to be created recursively
 default['s3_folder_dir'] = TEMP_DIR
 
 # by default, no gems set to grabbed from s3
@@ -32,55 +29,22 @@ default['s3-install-on-setup'] = []
 default['install-on-setup'] = []
 
 # set the default version of the aws-sdk to nil so it automatically pulls the
-# newest one
+# newest one by default
 default['aws-sdk-version'] = nil
 
 # set the version of ruby that will be installed
 set['opsworks']['ruby_version'] = '1.9.3'
 set['opsworks']['ruby_stack'] = 'ruby'
 
-# set default user and group to be the current one (root during recipe execution)
+# the recipes default to using the identity that OpsWorks uses while running
+# recipes, which is the root identity currently
 default['user'] = `id -u -n`
 default['group'] = `id -g -n` 
 
-# set default runner location
+# set default runner location, a file that can be used to redeploy workers
+# while on the instance
 default['runner_folder_dir'] = '/home/runner'
 default['runner_file_dir'] = "#{node['runner_folder_dir']}/runner.rb"
 
 # default name for each worker (once the process is forked)
 default['worker_process_name'] = 'aws-rf-worker'
-
-# Commonly used functions
-
-def new_dir(dir, mode, user, group)
-  new_dir = directory dir do
-    mode mode
-    owner user
-    group group
-    action :nothing
-    recursive true
-
-    not_if do
-      ::File.exists?(dir)
-    end
-  end
-  
-  new_dir.run_action(:create)
-end
-
-def new_file(dir, mode, user, group, content = nil, overwrite = false)
-  new_file = file dir do
-    mode mode
-    owner user
-    group group
-    action :nothing
-    content content
-
-  end
-  
-  if overwrite
-    new_file.run_action(:create)
-  else
-    new_file.run_action(:create_if_missing)
-  end
-end
